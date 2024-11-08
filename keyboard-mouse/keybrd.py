d66d7e75b8d01a90de9bf52ce7e9217544fe4d9c
The1stBurb <eli.cooper@ucas-edu.net>
2024-11-07 18:37:21 -0700

    hehey

--- keyboard-mouse/keybrd.py
+++ keyboard-mouse/keybrd.py
@@ -1,7 +1,7 @@
 import keyboard as kb
 from random import randint
-kys="qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM".split()
-kys2="qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM".split()
+kys=list("qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM")
+kys2=list("qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM")
 print(kys2)
 for i in kys:
     ch2=kys[randint(0,len(kys)-1)]
@@ -10,12 +10,12 @@ for i in kys:
     kys2.remove(ch)
     kb.remap_key(ch, ch2)
 kb.wait(hotkey="esc")
-quit()
-# kb.add_abbreviation(source_text, replacement_text, match_suffix=False, timeout=2)
-# kb.add_abbreviation(source_text, replacement_text, match_suffix=False, timeout=2)
-# kb.add_abbreviation(source_text, replacement_text, match_suffix=False, timeout=2)
-rec=kb.record(until="esc")
-kb.play(rec,speed_factor=100)
-print("yops")
-print(kb.read_event())
-#QWERTNUISPASDFGHJKWZXCVEMqwertnuispasdfghjkwzxcvem
+# quit()
+# # kb.add_abbreviation(source_text, replacement_text, match_suffix=False, timeout=2)
+# # kb.add_abbreviation(source_text, replacement_text, match_suffix=False, timeout=2)
+# # kb.add_abbreviation(source_text, replacement_text, match_suffix=False, timeout=2)
+# rec=kb.record(until="esc")
+# kb.play(rec,speed_factor=100)
+# print("yops")
+# print(kb.read_event())
+# #Rtq rg lqis isj'l igiji ypry iggo!
