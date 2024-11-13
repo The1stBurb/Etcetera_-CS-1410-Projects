973de5d05f590e5855f4b1c29033532d2dba5ba7
The1stBurb <eli.cooper@ucas-edu.net>
2024-11-13 11:39:10 -0700

    jhsdgf

---
+++ exceptions/main.py
@@ -0,0 +1,26 @@
+#cathc 0 div, file not found, vlaue error, type error, index error
+# num=int(input("tell me a num: "))
+# num2=int(input("tell me another num: "))
+# print(f"{str(num)} plus {str(num2)} is {num+num2}")
+# try:
+#     print(sakjgdsa)
+# except TypeError:
+#     print("ferg")
+def tryr(t):
+    try:
+        num=int(input(t))
+    except:
+        print("Thats wrong!")
+        num=tryr(t)
+    return num
+num=tryr("tell me a num: ")
+num2=tryr("tell me another num: ")
+print(f"{str(num)} plus {str(num2)} is {num+num2}")
+try:#try this code
+    pass
+except TypeError:#if error run this
+    pass
+else:#if no error run this
+    pass
+finally:#always runs
+    pass
