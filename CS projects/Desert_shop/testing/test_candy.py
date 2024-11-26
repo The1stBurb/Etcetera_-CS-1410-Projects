582d90dbf25665b40952ab3371b69912c0f65a18
The1stBurb <eli.cooper@ucas-edu.net>
2024-11-26 11:05:53 -0700

    saf

--- CS projects/Desert_shop/testing/test_candy.py
+++ CS projects/Desert_shop/testing/test_candy.py
@@ -4,5 +4,5 @@ def test_candy():
     assert tstcnd.gt_nm()==tstcnd.nm
     assert tstcnd.cost()==tstcnd.quan*tstcnd.pp_
     assert tstcnd.get_quan()==tstcnd.quan
-    assert tstcnd.calctx()==tstcnd.cost()*tstcnd.txp
+    assert tstcnd.calctx()==round(tstcnd.cost()*tstcnd.txp*100)/100
     assert tstcnd.cost()==tstcnd.quan*tstcnd.pp_
--- CS projects/Desert_shop/testing/test_sunday.py
+++ CS projects/Desert_shop/testing/test_sunday.py
@@ -1,8 +1,8 @@
 from desert import sunday
 def test_sunday():
-    tstcnd=sunday("vanil sprink",2,1,"sprink")
+    tstcnd=sunday("vanil sprink",2,1,"sprink",1)
     assert tstcnd.gt_nm()==tstcnd.nm
-    assert tstcnd.cost()==tstcnd.quan*tstcnd.pp_
+    # assert tstcnd.cost()==tstcnd.quan*tstcnd.pp_
     assert tstcnd.get_quan()==tstcnd.quan
-    assert tstcnd.calctx()==tstcnd.cost()*tstcnd.txp
+    assert tstcnd.calctx()==round(tstcnd.cost()*tstcnd.txp*100)/100
     assert tstcnd.cost()==tstcnd.quan*tstcnd.pp_+tstcnd.ppt
